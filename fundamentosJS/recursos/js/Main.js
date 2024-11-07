fd73007c824deb6082598ecf2ade067c2ba9e85f
Duban Jimenez <duvanj390@gmail.com>
2024-11-07 17:40:09 -0500

    se crea el proyceto con los ejemplos de los fundamento de javascript

---
+++ fundamentosJS/recursos/js/Main.js
@@ -0,0 +1,51 @@
+
+/*Metodo mas usuado*/
+document.getElementById("demo").innerHTML ="hola javascript";
+document.getElementById("demo").style.fontSize= "35px";
+
+/*Salidas de datos*/
+window.alert(5+2);
+console.log(6+2);
+
+/*declaracion*/
+let a , b ,c;
+a=5;
+b=6;
+c=a+b;
+window.alert(c);
+document.getElementById("demo").innerHTML=c;
+
+/*VARIABLES*/
+let p,q,r;
+p=7;
+q=5;
+const m=8;
+r=p+q+m;
+
+document.getElementById("demo1").innerHTML=r;
+
+
+/*tipos de datos*/
+let num= 24; //numero entero
+let name = "duban jimenez"; //cadena
+let kn = 14.5;//numero decimal
+let iscolombiano = true;//booleana
+let obj = {firstname:  "ana", lastname: "perez"};//objeto
+
+let y =17 +"poo";
+let unir ="17"+"poo";
+window.alert(unir)
+window.alert(y)
+
+/*definicipon de funcion*/
+function myFuncion(){
+    document.getElementById("demo2").innerHTML="new paragraph"
+}
+
+
+
+
+
+
+
+
